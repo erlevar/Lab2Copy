cbcef7415c6b4dfad17628785f98823d691915dc
guidotti1 <54955400+guidotti1@users.noreply.github.com>
2019-10-17 14:37:26 -0400

    Update extraFunctions.cpp

--- extraFunctions.cpp
+++ extraFunctions.cpp
@@ -24,37 +24,12 @@ void getTriggers(XMLNode node, vector<trigger> & triggers)
 
 void separateWords(string s, vector<string> & separated)
 {
-    /*
-    vector<char> newWord;
-    for (int j = 0; j<s.length(); j++)
-        {
-        //enters all non null characters into a character array
-        if(s[j] != '\0')
-            {
-            newWord.push_back(s[j]);
-            }
-        else
-            {
-            //if null character is found immediately just skip
-            if (newWord.size() == 0)
-                {
-                continue;
-                }
-            //otherwise we make a cstring and fill it with the character array, add the terminating value '\0', then add that to the full name vector.
-            char cString[newWord.size()+1];
-            for (int k = 0; k<newWord.size(); k++)
-                {
-                cString[k] = newWord[k];
-                }
-            cString[newWord.size()] = '\0';
-            fullName.push_back(cString);
-            newWord.clear();
-            }
-        }
-        */
-    stringstream stream(s);
+    istringstream ss(s);
     string word;
-    getline(stream, word, ' ');
-    separated.push_back(word);
+    do
+        {
+        ss >> word;
+        separated.push_back(word);
 
+        }while(ss);
 }
