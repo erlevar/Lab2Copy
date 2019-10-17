c7e733b4e8911fefc8b0963e4d7afa448607fb35
guidotti1 <54955400+guidotti1@users.noreply.github.com>
2019-10-17 14:05:02 -0400

    Update room.h

--- room.h
+++ room.h
@@ -1,39 +1,120 @@
-#ifndef ROOM_H
-#define ROOM_H
+#include "room.h"
 
-#include <vector>
-#include "xmlParser.h"
-#include "item.h"
-#include "border.h"
-#include "container.h"
-#include "trigger.h"
-#include "creature.h"
-using namespace std;
-
-
-class room
+room::room()
 {
-public :
-    room();
-    room(XMLNode node);
-    void getBorders(XMLNode node);
-    void getContainers(XMLNode node);
-    void getCreatures(XMLNode node);
-    string getName();
-    void readDescription();
-    string checkBorders(string input);
-    item checkItems(string input);
+    name, description, type = "";
+}
+
+room::room(XMLNode node)
+{
+    name, description, type = "";
+    //room information
+    //room type
+    XMLNode roomTypeNode = node.getChildNode("type");
+    if (!roomTypeNode.isEmpty())
+        {
+        type = roomTypeNode.getText();
+        }
+
+    // Get the room name
+    XMLNode nameNode = node.getChildNode("name");
+    if (!nameNode.isEmpty())
+        {
+        name = nameNode.getText();
+        }
+
+    //Get the room description
+    XMLNode descriptionNode = node.getChildNode("description");
+    if (!descriptionNode.isEmpty())
+        {
+        description = descriptionNode.getText();
+        }
+
+    cout << "(room information)" << endl;
+    cout << "Name of the room is : " << name << endl;
+    cout << "Description of the room is : " << description << endl;
+    cout << "Type of the room is : " <<  type << endl;
+
+    getItems(node, items);
+    getTriggers(node, triggers);
+    getBorders(node);
+    getContainers(node);
+    getCreatures(node);
+
+}
+
+void room::getBorders (XMLNode node)
+{
+    int numberBorders = node.nChildNode("border");
+    for (int nBorders = 0; nBorders < numberBorders; nBorders++)
+        {
+            XMLNode borderNode = node.getChildNode("border", nBorders);
+            border newBorder(borderNode);
+            borders.push_back(newBorder);
+        }
+}
 
 
-private :
-    string name;
-    string description;
-    string type;
-    vector<item> items;
-    vector<trigger> triggers;
-    vector<border> borders;
-    vector<container> containers;
-    vector<creature> creatures;
-};
+void room::getContainers(XMLNode node)
+{
+    int numberContainers = node.nChildNode("container");
+    for (int nContainers = 0; nContainers < numberContainers; nContainers++)
+        {
+        XMLNode containerNode = node.getChildNode("container", nContainers);
+        container newContainer(containerNode);
+        containers.push_back(newContainer);
+        }
+}
 
-#endif
+
+void room::getCreatures(XMLNode node)
+{
+    int numberCreatures = node.nChildNode("creature");
+    for (int nCreatures = 0; nCreatures < numberCreatures; nCreatures++)
+        {
+        XMLNode creatureNode = node.getChildNode("creature", nCreatures);
+        creature newCreature(creatureNode);
+        creatures.push_back(newCreature);
+
+        }
+}
+
+string room::getName()
+{
+    return name;
+}
+
+void room::readDescription()
+{
+    cout << description << endl;
+    return;
+}
+
+string room::checkBorders(string input)
+{
+    for (int i = 0; i<borders.size(); i++)
+        {
+        string direction = borders[i].getDirection();
+        if (input == direction)
+            {
+            return borders[i].getName();
+            }
+        }
+    string noBorder = "dummy";
+    return noBorder;
+}
+
+item room::checkItems(string input)
+{
+    for (int i = 0; i<items.size(); i++)
+        {
+        string itemName = items[i].getName();
+        if (input == itemName)
+            {
+            return items[i];
+            }
+        }
+    string dummy = "dummy";
+    item emptyItem(dummy);
+    return emptyItem;
+}
