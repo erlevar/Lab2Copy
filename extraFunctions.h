0c9218498e3eb31883542b4e466cc7a94b9efea7
guidotti1 <54955400+guidotti1@users.noreply.github.com>
2019-10-17 14:27:19 -0400

    Update extraFunctions.h

--- extraFunctions.h
+++ extraFunctions.h
@@ -6,4 +6,4 @@ using namespace std;
 
 void getItems(XMLNode node, vector<item> & items);
 void getTriggers(XMLNode node, vector<trigger> & triggers);
-void separateWords(string s, vector<string> & fullName);
+void separateWords(string s, vector<string> & separated);
