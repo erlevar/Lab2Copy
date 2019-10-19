dc3a9bf0e7fdf546566e1421c9534e33eff693ac
guidotti1 <54955400+guidotti1@users.noreply.github.com>
2019-10-19 14:56:54 -0400

    Update trigger.h

--- trigger.h
+++ trigger.h
@@ -2,21 +2,30 @@
 #define TRIGGER_H
 
 #include "condition.h"
+#include "item.h"
 #include "xmlParser.h"
 #include <iostream>
+#include <map>
 using namespace std;
 
 class trigger
 {
 public:
     trigger();
-    trigger(XMLNode node);
+    trigger(string s);
+    trigger(XMLNode node, map<string, item> itemMap);
+    string getCommand();
+    string getPrint();
+    string getType();
+    string getAction();
+    void checkCondition();
 
 private :
     string type;
     string command;
     string print;
     string action;
+    bool activated;
     condition triggerCondition;
 
 };
