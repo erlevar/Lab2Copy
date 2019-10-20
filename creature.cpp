c190b74a486ea669d7f36158fa03c0936ea86d1f
guidotti1 <54955400+guidotti1@users.noreply.github.com>
2019-10-20 15:11:35 -0400

    Update creature.cpp

--- creature.cpp
+++ creature.cpp
@@ -16,14 +16,9 @@ creature::creature(XMLNode node)
         vulnerability = creatureVulnerabilityNode.getText();
         }
 
-    cout << "(creature information)"<<endl;
-    cout << "creature name : "<< name << endl;
-    cout << "creature vulnerability : " << vulnerability << endl;
-    cout << "attack for creature : " << endl;
     XMLNode attackNode = node.getChildNode("attack");
     attack tempAttack(attackNode);
     creatureAttack = tempAttack;
-    cout << "trigger for creature : " << endl;
     XMLNode triggerNode = node.getChildNode("trigger");
     trigger tempTrigger(triggerNode);
     creatureTrigger = tempTrigger;
