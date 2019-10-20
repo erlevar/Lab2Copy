58e2f385a4f9ee3c37551dc7d3b9668484ea9cc6
guidotti1 <54955400+guidotti1@users.noreply.github.com>
2019-10-20 16:34:25 -0400

    Update room.cpp

--- room.cpp
+++ room.cpp
@@ -226,6 +226,7 @@ room& room::operator=(const room& r)
     for (int i = 0; i < r.items.size(); i++)
         {
         items.push_back(r.items[i]);
+        cout << "r.items[i] " << r.items[i].getName() << endl;
         }
     for (int i = 0; i < r.triggers.size(); i++)
         {
