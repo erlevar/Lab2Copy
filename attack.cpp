55d712f50383b9630492f0b75b674f740f90d32e
guidotti1 <54955400+guidotti1@users.noreply.github.com>
2019-10-20 15:10:52 -0400

    Update attack.cpp

--- attack.cpp
+++ attack.cpp
@@ -15,16 +15,12 @@ attack::attack(XMLNode node)
         print = printNode.getText();
         }
     int numberActions = node.nChildNode("action");
-    cout << "actions for attack " << endl;
     for (int nActions = 0; nActions < numberActions; nActions ++)
         {
         XMLNode actionNode = node.getChildNode("action", nActions);
         string actionInfo = actionNode.getText();
         actions.push_back(actionInfo);
-        cout << "actionInfo " << actionInfo << endl;
         }
-
-    cout << "condition for attack " <<endl;
     XMLNode conditionNode = node.getChildNode("condition");
     condition tempCondition(conditionNode);
     attackCondition = tempCondition;
