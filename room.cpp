a4829e35d0726facba9605d27411880bd25d3fe8
guidotti1 <54955400+guidotti1@users.noreply.github.com>
2019-10-21 14:44:49 -0400

    Update room.cpp

--- room.cpp
+++ room.cpp
@@ -1,138 +1,48 @@
-#include "room.h"
+#include "player.h"
 
-room::room()
+player::player(room startLocation)
 {
-    name, description, type = "";
+    current = startLocation;
 }
 
-room::room(XMLNode node)
+room player::currentLocation()
 {
-    name, description, type = "";
-    //room information
-    //room type
-    XMLNode roomTypeNode = node.getChildNode("type");
-    if (!roomTypeNode.isEmpty())
-        {
-        type = roomTypeNode.getText();
-        }
-
-    // Get the room name
-    XMLNode nameNode = node.getChildNode("name");
-    if (!nameNode.isEmpty())
-        {
-        name = nameNode.getText();
-        }
-
-    //Get the room description
-    XMLNode descriptionNode = node.getChildNode("description");
-    if (!descriptionNode.isEmpty())
-        {
-        description = descriptionNode.getText();
-        }
-    setItems(node);
-    for (int i = 0; i < items.size(); i++)
-        {
-        items[i].updateOwner(name);
-        //itemMap[items[i].getName()] = items[i];
-        }
-    setTriggers(node);
-    setBorders(node);
-    setContainers(node);
-    setCreatures(node);
-
+    return current;
 }
 
-void room::setBorders (XMLNode node)
+void player::updateLocation(room newLocation)
 {
-    int numberBorders = node.nChildNode("border");
-    for (int nBorders = 0; nBorders < numberBorders; nBorders++)
+    current = newLocation;
+}
+
+void player::readInventory()
+{
+    if (inventory.size() == 0)
         {
-            XMLNode borderNode = node.getChildNode("border", nBorders);
-            border newBorder(borderNode);
-            borders.push_back(newBorder);
+        cout << "Inventory : empty" << endl;
+        return;
         }
-}
-
-
-void room::setContainers(XMLNode node)
-{
-    int numberContainers = node.nChildNode("container");
-    for (int nContainers = 0; nContainers < numberContainers; nContainers++)
+    for (int i = 0; i < inventory.size(); i ++)
         {
-        XMLNode containerNode = node.getChildNode("container", nContainers);
-        container newContainer(containerNode);
-        containers.push_back(newContainer);
+        inventory[i].readName();
+
         }
-}
-
-
-void room::setCreatures(XMLNode node)
-{
-    int numberCreatures = node.nChildNode("creature");
-    for (int nCreatures = 0; nCreatures < numberCreatures; nCreatures++)
-        {
-        XMLNode creatureNode = node.getChildNode("creature", nCreatures);
-        creature newCreature(creatureNode);
-        creatures.push_back(newCreature);
-        }
-}
-
-void room::setTriggers(XMLNode node)
-{
-    int numberTriggers = node.nChildNode("trigger");
-    for (int nTriggers = 0; nTriggers < numberTriggers; nTriggers++)
-        {
-            XMLNode triggerNode=node.getChildNode("trigger", nTriggers);
-            trigger newTrigger(triggerNode);
-            triggers.push_back(newTrigger);
-        }
-}
-
-
-void room::setItems(XMLNode node)
-{
-    int numberItems = node.nChildNode("item");
-    for (int nItems = 0; nItems < numberItems; nItems++)
-        {
-        XMLNode itemNode = node.getChildNode("item", nItems);
-        item newItem(itemNode);
-        items.push_back(newItem);
-        }
-}
-
-string room::getName()
-{
-    return name;
-}
-
-void room::readDescription()
-{
-    cout << description << endl;
     return;
 }
 
-string room::checkBorders(string input)
+void player::addItem(item newItem)
 {
-    for (int i = 0; i<borders.size(); i++)
-        {
-        string direction = borders[i].getDirection();
-        if (input == direction)
-            {
-            return borders[i].getName();
-            }
-        }
-    string noBorder = "dummy";
-    return noBorder;
+    inventory.push_back(newItem);
 }
 
-item room::checkItems(string input)
+item player::checkItems(string input)
 {
-    for (int i = 0; i<items.size(); i++)
+    for (int i = 0; i<inventory.size(); i++)
         {
-        string itemName = items[i].getName();
+        string itemName = inventory[i].getName();
         if (input == itemName)
             {
-            return items[i];
+            return inventory[i];
             }
         }
     string dummy = "dummy";
@@ -140,145 +50,224 @@ item room::checkItems(string input)
     return emptyItem;
 }
 
-container room::checkContainers(string input)
+void player::removeItem(string itemName)
 {
-    for (int i = 0; i < containers.size(); i++)
-        {
-        string containerName = containers[i].getName();
-        if (input == containerName)
-            {
-            return containers[i];
-            }
-        }
-    string dummy = "dummy";
-    container emptyContainer(dummy);
-    return emptyContainer;
-}
-
-trigger room::checkTriggersByCommand(string input)
-{
-    for (int i = 0; i<triggers.size(); i++)
-        {
-        string triggerName = triggers[i].getCommand();
-        if (input == triggerName)
-            {
-            return triggers[i];
-            }
-        }
-    string dummy = "dummy";
-    trigger emptyTrigger(dummy);
-    return emptyTrigger;
-}
-
-
-void room::removeItem(string itemName)
-{
-    vector<item>::iterator iter;
-    for(iter = items.begin(); iter != items.end(); ++iter )
+    std::vector<item>::iterator iter;
+    for(iter = inventory.begin(); iter != inventory.end(); ++iter )
     {
         if((*iter).getName() == itemName)
         {
-            items.erase(iter);
+            inventory.erase(iter);
             break;
         }
     }
 }
 
-void room::removeContainer(string containerName)
+player& player::operator=(const player & p)
 {
-    vector<container>::iterator iter;
-    for (iter = containers.begin(); iter!= containers.end(); ++iter)
+    current = p.current;
+    for (int i =0; i < p.inventory.size(); i++)
         {
-        if ((*iter).getName() == containerName)
-            {
-                containers.erase(iter);
-                break;
-            }
-        }
-}
-
-
-void room::addItem(item newItem)
-{
-    items.push_back(newItem);
-}
-
-void room::addContainer(container newContainer)
-{
-    containers.push_back(newContainer);
-}
-
-
-void room::getRoomTriggerCommands(vector<string> & commands)
-{
-    for (int i = 0; i < triggers.size(); i++)
-        {
-        commands.push_back(triggers[i].getCommand());
-        }
-}
-
-string room::getRoomType()
-{
-    return type;
-}
-
-void room::readItems()
-{
-    cout << "Names of items in the room : " << endl;
-    for (int i = 0; i<items.size(); i++)
-        {
-            cout << items[i].getName() << endl;
-        }
-}
-
-
-room& room::operator=(const room& r)
-{
-    name = r.name;
-    type = r.type;
-    description = r.description;
-    items.clear();
-    triggers.clear();
-    borders.clear();
-    creatures.clear();
-    containers.clear();
-    for (int i = 0; i < r.items.size(); i++)
-        {
-        items.push_back(r.items[i]);
-        }
-    for (int i = 0; i < r.triggers.size(); i++)
-        {
-        triggers.push_back(r.triggers[i]);
-        }
-    for (int i = 0; i < r.borders.size(); i++)
-        {
-        borders.push_back(r.borders[i]);
-        }
-    for (int i = 0; i < r.creatures.size(); i++)
-        {
-        creatures.push_back(r.creatures[i]);
-        }
-    for (int i = 0; i < r.containers.size(); i++)
-        {
-        containers.push_back(r.containers[i]);
+        inventory.push_back(p.inventory[i]);
         }
     return *this;
 }
 
-void room::readBorders()
+void player::moveToBorder(map<string, room> roomMap, bool & changedRooms, string input)
 {
-    for (int i = 0; i<borders.size(); i++)
+    string passDirection;
+    if (input == "n")
         {
-            cout << "Border.name " << borders[i].getName() << endl;
-            cout << "Border.direction " << borders[i].getDirection() << endl;
+        passDirection = "north";
+        }
+    else if (input == "s")
+        {
+        passDirection = "south";
+        }
+    else if (input == "e")
+        {
+        passDirection = "east";
+        }
+    else if (input == "w")
+        {
+        passDirection = "west";
+        }
+    room current = currentLocation();
+    string returnRoomName = current.checkBorders(passDirection);
+    if (returnRoomName == "dummy")
+        {
+        cout << "Can't go that way"<<endl;
+        }
+    else
+        {
+        cout << "The name of the room to that direction is " << returnRoomName << endl;
+        cout << "You enter this room" << endl;
+        changedRooms = true;
+        room newLocation = roomMap[returnRoomName];
+        updateLocation(newLocation);
+        }
+
+}
+
+void player::userTakeItem(room & current, string input)
+{
+    item returnItem = current.checkItems(input);
+    vector<container> roomContainers;
+    current.getContainers(roomContainers);
+
+    bool itemPresent = false;
+    bool itemInRoom = false;
+    bool itemInContainer = false;
+    container presentContainer;
+    item presentItem;
+
+    for (int i =0; i < roomContainers.size(); i++)
+        {
+        vector<item> itemsInContainer;
+        roomContainers[i].getItems(itemsInContainer);
+        for (int j = 0; j < itemsInContainer.size(); j++)
+            {
+            if (itemsInContainer[j].getName() == input)
+                {
+                itemInContainer = true;
+                presentContainer = roomContainers[i];
+                presentItem = itemsInContainer[j];
+                break;
+                }
+            }
+        }
+
+    if (returnItem.getName() == input)
+        {
+        itemInRoom = true;
+        }
+
+    item checkInventory = checkItems(input);
+    if (checkInventory.getName() == "dummy")
+        {
+        if ((itemInContainer == false) && (itemInRoom == false))
+            {
+            cout << "No such item to pick up" << endl;
+            }
+        else if (itemInRoom == true)
+            {
+            cout << returnItem.getName() << " added to inventory " << endl;
+            returnItem.updateOwner("inventory");
+            current.removeItem(returnItem.getName());
+            addItem(returnItem);
+            }
+        else if (itemInContainer == true)
+            {
+            cout << presentItem.getName() << " added to inventory " << endl;
+            presentItem.updateOwner("inventory");
+            presentContainer.removeItem(presentItem.getName());
+            current.removeContainer(presentContainer.getName());
+            current.addContainer(presentContainer);
+            addItem(presentItem);
+            }
+       }
+    else
+        {
+        cout << "You already have that item " << endl;
         }
 }
 
-void room::getContainers(vector<container> & returnContainers)
+void player::userReadItem(string input)
 {
-    for (int i = 0; i<containers.size(); i++)
+    room currnet = currentLocation();
+    item returnItem = checkItems(input);
+    if (returnItem.getName() == "dummy")
         {
-        returnContainers.push_back(containers[i]);
+        cout << "No such item in your inventory to be read " << endl;
         }
-    return;
+    else
+        {
+        returnItem.readWriting();
+        }
 }
+
+void player::userDropItem(room & current, string input)
+{
+    item returnItem = checkItems(input);
+    if (returnItem.getName() == "dummy")
+        {
+        cout << "No such item in your inventory to drop" << endl;
+        }
+    else
+        {
+        cout << "You dropped the " << returnItem.getName() << endl;
+        returnItem.updateOwner(current.getName());
+        current.addItem(returnItem);
+        removeItem(returnItem.getName());
+        }
+}
+
+void player::isAtExit(bool & foundExit)
+{
+    string roomType = current.getRoomType();
+    if (roomType == "exit")
+        {
+        cout << "VICTORY!!!"<<endl;
+        foundExit = true;
+        }
+    else
+        {
+        cout << "You have not made it to the exit yet " << endl;
+        }
+}
+
+void player::userOpenContainer(string input)
+{
+    container returnContainer = current.checkContainers(input);
+    if (returnContainer.getName() == "dummy")
+        {
+        cout << "No such container in the room to open" << endl;
+        }
+    else
+        {
+        cout << "You open the " << returnContainer.getName() <<endl;
+        returnContainer.readItems();
+        }
+}
+
+void player::userTurnonItem(string input)
+{
+    item returnItem = checkItems(input);
+    if (returnItem.getName() == "dummy")
+        {
+        cout << "No such item in your inventory to turn on " << endl;
+        }
+    else
+        {
+        cout << "You turn on the " << returnItem.getName() << endl;
+        returnItem.activateTurnon();
+        removeItem(returnItem.getName());
+        addItem(returnItem);
+        }
+}
+
+void player::userPutItemInContainer(room & current, string itemName, string containerName)
+{
+    item returnItem = checkItems(itemName);
+    container returnContainer = current.checkContainers(containerName);
+    if (returnItem.getName() == "dummy")
+        {
+        cout << "No such item in your inventory to be placed in the container " << endl;
+        return;
+        }
+
+    if (returnContainer.getName() == "dummy")
+        {
+        cout << "No such container in the room to place the item in " << endl;
+        return;
+        }
+
+    cout << "You put the " << returnItem.getName() << " in the " << returnContainer.getName() << endl;
+    removeItem(itemName);
+    returnContainer.addItem(returnItem);
+    current.removeContainer(returnContainer.getName());
+    curretn.addContainer(returnContainer);
+
+}
+
+
