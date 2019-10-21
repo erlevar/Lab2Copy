f8e1776c67a9c15c0a0c307a6e100e53bced3392
guidotti1 <54955400+guidotti1@users.noreply.github.com>
2019-10-21 15:46:45 -0400

    Update xmlTesting.cpp

--- xmlTesting.cpp
+++ xmlTesting.cpp
@@ -81,6 +81,7 @@ int main (int argc, char ** argv) {
                 roomContainers[i].getContainerTriggerComands(containerTriggerCommands);
                 for (int j = 0; j<containerTriggerCommands.size(); j++)
                     {
+                        cout << "containerTriggerCommands[j]: " << containerTriggerCommands[j] << endl;
                         if(userinput == containerTriggerCommands[j])
                             {
                             triggerPresentContainer = roomContainers[t];
