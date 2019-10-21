c55fd8d3b01ac91c896576c2da3db527c987cccc
guidotti1 <54955400+guidotti1@users.noreply.github.com>
2019-10-21 15:45:31 -0400

    Update xmlTesting.cpp

--- xmlTesting.cpp
+++ xmlTesting.cpp
@@ -89,6 +89,7 @@ int main (int argc, char ** argv) {
                             }
                     }
             }
+        cout << "end of roomContainers for loop " << endl;
 
 
         if ((triggersPresentInRoom) && !(triggersPresentInContainers))
