c40bfda9cbd382a03fab8271523b18cf20c5af61
guidotti1 <54955400+guidotti1@users.noreply.github.com>
2019-10-21 14:45:25 -0400

    Update container.h

--- container.h
+++ container.h
@@ -24,6 +24,7 @@ public:
     item checkItems(string input);
     void getItems(vector<item> & returnItems);
     void removeItem(string itemName);
+    void addItem(item newItem);
 
 private :
     string name;
