26e7db25d479e1773709743548ea5edadc78e68d
guidotti1 <54955400+guidotti1@users.noreply.github.com>
2019-10-21 14:23:47 -0400

    Update player.h

--- player.h
+++ player.h
@@ -12,11 +12,20 @@ public:
     player(room startLocation);
     room currentLocation();
     void updateLocation(room newLocation);
-    void readInventory();
-    void takeItem(item newItem);
+    void addItem(item newItem);
     item checkItems(string s);
-    void dropItem(string s);
+    void removeItem(string s);
     player& operator=(const player & p);
+
+    //all for executing user commands
+    void readInventory(); //i commands
+    void moveToBorder(string input);  //n, s, e, w commands
+    void userTakeItem(room & current, string input); //take (item) commands
+    void userReadItem(string input); //read (item) commands
+    void userDropItem(room & current, string input); //drop (item) commands
+    void isAtExit(bool & foundExit); //used for open exit commands
+    void userOpenContainer(string input); //used for open container commands
+    void userTurnonItem (string input); //used for turn on (item) commands
 
 private :
     room current;
