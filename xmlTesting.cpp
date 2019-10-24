068917c2814d555159cfef4eed6e1d934c0b71c4
guidotti1 <54955400+guidotti1@users.noreply.github.com>
2019-10-24 13:40:43 -0400

    Update xmlTesting.cpp

--- xmlTesting.cpp
+++ xmlTesting.cpp
@@ -110,7 +110,7 @@ int main (int argc, char ** argv) {
                                 string action = containerTrigger.getAction();
                                 vector<string> actionVect;
                                 separateWords(action, actionVect);
-                                roomContainers[i].updateStatus(actionVect[actionVect[actionVect.size()-1]);
+                                roomContainers[i].updateStatus(actionVect[actionVect.size()-1]);
                                 }
                             }
                         }
