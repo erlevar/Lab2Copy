c014b268c0792461571442911ce13313d3a5ac9e
guidotti1 <54955400+guidotti1@users.noreply.github.com>
2019-10-26 15:54:05 -0400

    Update player.h

--- player.h
+++ player.h
@@ -11,12 +11,19 @@ class player
 {
 public:
     player();
+    //constructor that initializes player current location to startLocation
     player(room startLocation);
+    //returns currentLocation
     room currentLocation();
+    //updates Location
     void updateLocation(room newLocation);
+    //updates inventory
     void addItem(item newItem);
+    //check user inventory by item name
     item checkItems(string s);
+    //remove item from user inventory, by name
     void removeItem(string s);
+    //overloaded assignment operator
     player& operator=(const player & p);
 
     //all for executing user commands
@@ -28,8 +35,8 @@ public:
     void isAtExit(bool & foundExit); //used for open exit commands
     void userOpenContainer(string input); //used for open container commands
     void userTurnonItem (string input); //used for turn on (item) commands
-    void userPutItemInContainer(room & current, string itemName, string containerName); //user for put (item) in (container) commands
-    void userAttackCreature(string creatureName, string itemName, room & current);
+    void userPutItemInContainer(room & current, string itemName, string containerName); //used for put (item) in (container) commands
+    void userAttackCreature(string creatureName, string itemName, room & current); //used for attack (creature) with (item) commands
 
 private :
     room current;
