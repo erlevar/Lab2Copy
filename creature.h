3cbcca3865f48dd26a10a642caf7646df2bef401
guidotti1 <54955400+guidotti1@users.noreply.github.com>
2019-10-26 15:50:07 -0400

    Update creature.h

--- creature.h
+++ creature.h
@@ -12,13 +12,18 @@ class creature
 {
 public :
     creature();
+    //"dummy" constructor, only used to return null values
     creature(string s);
+    //constructor that takes information from XML node to initialize data members
     creature(XMLNode node);
+    //accessor functions
     trigger getTrigger();
     string getName();
     attack getAttack();
     item getItem();
+    //removes item from the creature (this will happen, for example, when the creature is attacked and killed)
     void removeItem();
+    //change the creatureTrigger to newTrigger
     void updateTrigger(trigger newTrigger);
 private :
     string name;
