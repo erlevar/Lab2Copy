51b7f6c4e415f76fabc44513cd6777746f571ee5
guidotti1 <54955400+guidotti1@users.noreply.github.com>
2019-10-26 15:44:11 -0400

    Update condition.h

--- condition.h
+++ condition.h
@@ -3,18 +3,20 @@
 
 #include "xmlParser.h"
 #include <iostream>
-#include <map>
 using namespace std;
 
 class condition
 {
 public:
     condition();
+    //constructor that takes information from XMLNode
     condition(XMLNode node);
+    //accessor functions
     string getHas();
     string getObject();
     string getStatus();
     string getOwner();
+    //checks if condition is fulfilled
     bool checkCondition();
 
 private:
